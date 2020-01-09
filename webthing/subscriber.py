00cfda7a1b201e12bc40ba89de37fa0da719328f
Pieter Moens <pieter.moens@ugent.be>
2020-01-09 16:51:35 +0100

    Python2.7 compatibility without ABC

--- webthing/subscriber.py
+++ webthing/subscriber.py
@@ -1,39 +1,33 @@
 """High-level Subscriber base class implementation."""
 
-from abc import ABC, abstractmethod
 
-
-class Subscriber(ABC):
+class Subscriber:
     """Abstract Subscriber class."""
 
-    @abstractmethod
     def update(self):
         """Receive update from a Thing."""
-        pass
+        raise NotImplementedError
 
-    @abstractmethod
     def update_property(self, property_):
         """
         Receive update from a Thing about an Property.
 
         :param property_: Property
         """
-        pass
+        raise NotImplementedError
 
-    @abstractmethod
     def update_action(self, action):
         """
         Receive update from a Thing about an Action.
 
         :param action: Action
         """
-        pass
+        raise NotImplementedError
 
-    @abstractmethod
     def update_event(self, event):
         """
         Receive update from a Thing about an Event.
 
         :param event: Event
         """
-        pass
+        raise NotImplementedError
